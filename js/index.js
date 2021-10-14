2a1fb56b0a8271df830c4fe207efca11bb603632
LillyTBui <>
2021-10-14 21:00:01 +0200

    forside
    har laget forside som ligner på prototypen.

---
+++ js/index.js
@@ -0,0 +1,56 @@
+/* Knapper */
+const start = document.querySelector("#btn-start");
+const backBtn = document.querySelectorAll(".btn-back");
+const howToPlay = document.querySelector("#btn-how-to-play");
+const credits = document.querySelector("#btn-credits");
+const settings = document.querySelector("#btn-settings");
+
+const menu = document.querySelector(".menu");
+const carousel = document.querySelector("#carousel-container");
+const howToPlayBlock = document.querySelector(".how-to-play")
+const creditsBlock = document.querySelector(".credits");
+const settingsBlock = document.querySelector(".settings")
+
+/* surfebrett */
+const surfGreen = document.querySelector("#surfboard-green");
+const surfGray = document.querySelector("#surfboard-gray");
+const surfPink = document.querySelector("#surfboard-pink");
+
+
+
+start.onclick = ()=>{
+    menu.style.display = "none";
+    carousel.style.display = "block";
+}
+
+howToPlay.onclick = ()=>{
+    menu.style.display = "none";
+    howToPlayBlock.style.display = "flex";
+}
+
+settings.onclick = ()=>{
+    menu.style.display = "none";
+    settingsBlock.style.display = "flex";
+}
+
+credits.onclick = ()=>{
+    menu.style.display = "none";
+    creditsBlock.style.display = "flex";
+}
+
+for (buttons of backBtn){
+    buttons.addEventListener('click', ()=>{
+        menu.style.display = "flex";
+        creditsBlock.style.display = "none";
+        settingsBlock.style.display = "none";
+        howToPlayBlock.style.display = "none";
+    } )
+}
+
+function toGame(){
+    window.location.href ="game.html";
+}
+
+surfGreen.addEventListener("click", toGame);
+surfGray.addEventListener("click", toGame);
+surfPink.addEventListener("click", toGame);
