b748070dac290e2f92df23078e9f684738c980e9
8stian9 <92333444+8stian9@users.noreply.github.com>
2021-11-02 12:02:16 +0100

    Scaling, buttons
    Har rydda litt opp i koden. Endringer ført i changelog i gameSceneog discen. 2.11

--- js/scenes/endScene.js
+++ js/scenes/endScene.js
@@ -8,21 +8,33 @@ class EndScene extends Phaser.Scene {
 
 	create() {
         // Creating text on screen
-        this.add.text(this.cameras.main.width/3, this.cameras.main.height/2 - 160, 'Game over!', {fill: '#FFFFFF', fontSize: '40px'})
+        // this.add.text(this.cameras.main.width/3, this.cameras.main.height/2 - 160, 'Game over!', {fill: '#FFFFFF', fontSize: '40px'})
 
         // Creating buttons on screen
-        gameState.menu = this.add.image(100, 40, 'iconmenu').setScale(.3).setInteractive();
-        gameState.continue = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 - 80, 'iconcontinue').setScale(.3).setInteractive();
-        gameState.sound = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 , 'iconsound').setScale(.3).setInteractive();
-        gameState.settings = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 80, 'iconsettings').setScale(.3).setInteractive();
-        gameState.exit = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 160, 'iconexit').setScale(.3).setInteractive();
-        gameState.howto = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 240, 'iconhowtoplay').setScale(.3).setInteractive();
+        // gameState.menu = this.add.image(100, 40, 'iconmenu').setScale(.3).setInteractive();
+        // gameState.continue = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 - 80, 'iconcontinue').setScale(.3).setInteractive();
+        // gameState.sound = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 , 'iconsound').setScale(.3).setInteractive();
+        // gameState.settings = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 80, 'iconsettings').setScale(.3).setInteractive();
+        // gameState.exit = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 160, 'iconexit').setScale(.3).setInteractive();
+        // gameState.howto = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 240, 'iconhowtoplay').setScale(.3).setInteractive();
             
+        // gameState.settings = this.add.image(100, 40, 'iconsettings').setScale(.3).setInteractive();
+        
+        //For scaling
+        let scaleX = this.cameras.main.width / gameState.bgWave.width;
+        let scaleY = this.cameras.main.height / gameState.bgWave.height;
+        let scale = Math.max(scaleX, scaleY);
+
+        gameState.play = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 - 80, 'iconplay').setScale(scale / 3.6).setInteractive();
+        gameState.sound = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 , 'iconsound').setScale(scale / 3.6).setInteractive();
+        gameState.exit = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 80, 'iconexit').setScale(scale / 3.6).setInteractive();
+
+
 
         // Button functionality;
         // Hide buttons 
         var buttonsShowing = true;
-        gameState.menu.on('pointerup', () => {
+        gameState.play.on('pointerup', () => {
             if(buttonsShowing){
                 this.scene.bringToTop('GameScene');
                 buttonsShowing = false;
@@ -31,7 +43,7 @@ class EndScene extends Phaser.Scene {
                 buttonsShowing = true;
             }
          })  
-         gameState.menu.on('pointerover', () => {
+         gameState.play.on('pointerover', () => {
             if(!buttonsShowing){
                 this.scene.bringToTop('EndScene');
                 buttonsShowing = true;
@@ -39,8 +51,8 @@ class EndScene extends Phaser.Scene {
          })        
 
   
-        // Start new game by pressing the menu button again or the continue button
-        gameState.continue.on('pointerup', () => {
+        // Start new game by pressing play
+        gameState.play.on('pointerup', () => {
 			this.scene.stop('PauseScene');
 			this.scene.start('GameScene');
         })
@@ -59,9 +71,9 @@ class EndScene extends Phaser.Scene {
         })
 
         // Settings
-        gameState.settings.on('pointerup', () => {
+        // gameState.settings.on('pointerup', () => {
 
-        })
+        // })
 
         // Exit
         gameState.exit.on('pointerup', () => {
@@ -69,33 +81,34 @@ class EndScene extends Phaser.Scene {
         })
 
         // How to play
-        gameState.howto.on('pointerup', () => {
-            document.getElementsByClassName('box-menu')
-        })       
+        // gameState.howto.on('pointerup', () => {
+        //     document.getElementsByClassName('box-menu')
+        // })       
 
 
 
         // Button functions and styling shared by ALL buttons
        const endButtonList = [
-            gameState.menu,
-            gameState.continue,
+            // gameState.menu,
+            // gameState.continue,
             gameState.sound,
             gameState.settings,
             gameState.exit,
-            gameState.howto
+            // gameState.howto,
+            gameState.play
         ]
         endButtonList.forEach(btn => {
             btn.on('pointerover', () => {
-                btn.setScale(.4);
+                btn.setScale(scale /3);
                })
             btn.on('pointerout', () => {
-                btn.setScale(.3);
+                btn.setScale(scale / 3.6);
                })
             btn.on('pointerdown', () => {
-                btn.setScale(.45);
+                btn.setScale(scale / 2.7);
                })
             btn.on('pointerup', () => {
-                btn.setScale(.3);
+                btn.setScale(scale / 3.6);
                })
 
 
--- js/scenes/pauseScene.js
+++ js/scenes/pauseScene.js
@@ -7,28 +7,25 @@ class PauseScene extends Phaser.Scene {
     }
 
 	create() {
-        // Creating text on screen
-        this.add.text(this.cameras.main.width/3, this.cameras.main.height/2 - 160, 'Game paused', {fill: '#FFFFFF', fontSize: '40px'})
 
-        // Creating buttons on screen
-        gameState.menu = this.add.image(100, 40, 'iconmenu').setScale(.3).setInteractive();
-        gameState.continue = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 - 80, 'iconcontinue').setScale(.3).setInteractive();
-        gameState.sound = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 , 'iconsound').setScale(.3).setInteractive();
-        gameState.settings = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 80, 'iconsettings').setScale(.3).setInteractive();
-        gameState.exit = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 160, 'iconexit').setScale(.3).setInteractive();
-        gameState.howto = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 240, 'iconhowtoplay').setScale(.3).setInteractive();
+        let scaleX = this.cameras.main.width / gameState.bgWave.width;
+        let scaleY = this.cameras.main.height / gameState.bgWave.height;
+        let scale = Math.max(scaleX, scaleY);
+
+        gameState.settings = this.add.image(100, 40, 'iconsettings').setScale(scale / 3.6).setInteractive();
+        gameState.play = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 - 80, 'iconplay').setScale(scale / 3.6).setInteractive();
+        gameState.sound = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 , 'iconsound').setScale(scale / 3.6).setInteractive();
+        gameState.exit = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2 + 80, 'iconexit').setScale(scale / 3.6).setInteractive();
+        
             
 
         // Button functionality;
-        // Continue game by pressing the menu button again or the continue button
-        gameState.menu.on('pointerup', () => {
+        // Continue game by pressing play
+        gameState.play.on('pointerup', () => {
 			this.scene.stop('PauseScene');
 			this.scene.resume('GameScene');
         })
-        gameState.continue.on('pointerup', () => {
-			this.scene.stop('PauseScene');
-			this.scene.resume('GameScene');
-        })
+
         
         // Music: switch on or off 
         var musicPlaying = true;
@@ -43,52 +40,38 @@ class PauseScene extends Phaser.Scene {
             }
         })
 
-        // Settings
-        gameState.settings.on('pointerup', () => {
-
-        })
-
         // Exit
         gameState.exit.on('pointerup', () => {
             window.location.replace('/../../index.html');
         })
 
-        // How to play
-        gameState.howto.on('pointerup', () => {
-
-        })       
-
-
-
         // Button functions and styling shared by ALL buttons
        const pauseButtonList = [
-            gameState.menu,
-            gameState.continue,
             gameState.sound,
             gameState.settings,
             gameState.exit,
-            gameState.howto
+            gameState.play
         ]
         pauseButtonList.forEach(btn => {
             btn.on('pointerover', () => {
-                btn.setScale(.4);
+                btn.setScale(scale / 3);
                })
             btn.on('pointerout', () => {
-                btn.setScale(.3);
+                btn.setScale(scale / 3.6);
                })
             btn.on('pointerdown', () => {
-                btn.setScale(.45);
+                btn.setScale(scale / 2.7);
                })
             btn.on('pointerup', () => {
-                btn.setScale(.3);
+                btn.setScale(scale / 3.6);
                })
-
-
             });      
 
     }
-    update(){
 
+
+    update(){
+ 
     }
 }
 
